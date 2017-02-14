10f4b840e87724af17df23945edcec68ff62a772
Saša Zrna <sazrna@vsite.hr>
2017-02-14 14:42:00 +0100

    1.Dodane stranice: PrviPrigovor, OdgovorPrigovora, FabButton, TabPageIliTabView(nalaze se u PCL), 2.Dodan edite picture image za Usera 3. dodan fontawsome u PLC i Renderer za Android

---
+++ PrigovorHR/PrigovorHR/OdgovorPrigovora.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace PrigovorHR
+{
+    public partial class OdgovorPrigovora : ContentPage
+    {
+        public OdgovorPrigovora()
+        {
+        
+            InitializeComponent();
+
+            attach_pdf_font.Text = '\uf1c1'.ToString();
+            attach_pdf_font.TextColor = Color.Gray;
+
+
+            attach_photo_font.Text = '\uf030'.ToString();
+            attach_photo_font.TextColor = Color.Gray;
+
+            attach_voice_font.Text = '\uf041'.ToString();
+            attach_voice_font.TextColor = Color.Gray;
+
+            progres_memorije_attachmenta.Progress = 0.1;
+
+            dodani_pdf.Text = '\uf1c1'.ToString();
+            dodani_pdf.TextColor = Color.Gray;
+
+            send_font.Text = '\uf2c6'.ToString();
+            send_font.TextColor = Color.FromHex("#FF7e65");
+            send_font.FontSize = 55;
+        }
+    }
+}
---
+++ PrigovorHR/PrigovorHR/TabPageIliView.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace PrigovorHR
+{
+    public partial class TabPageIliView : ContentPage
+    {
+        public TabPageIliView()
+        {
+            InitializeComponent();
+            ZatPrigUnder.IsVisible = false;
+            ZatPrigStack.IsVisible = false;
+
+
+            //Paljenje taba aktivnih prigovora, gašenje taba zatvorenih prigovora
+
+            var AktPrigGestureRecognizer = new TapGestureRecognizer();
+            AktPrigGestureRecognizer.Tapped += (s, e) =>
+            {
+                AktPrigovorStack.IsVisible = true;
+                AktPrigUnder.IsVisible = true;
+                ZatPrigUnder.IsVisible = false;
+                ZatPrigStack.IsVisible = false;
+            };
+            AktPrigLabel.GestureRecognizers.Add(AktPrigGestureRecognizer);
+
+
+            //Paljenje taba zatvorenih prigovora, gašenje taba aktivnih prigovora
+
+            var ZatPrigGestureRecognizer = new TapGestureRecognizer();
+            ZatPrigGestureRecognizer.Tapped += (s, e) =>
+            {
+                AktPrigovorStack.IsVisible = false;
+                AktPrigUnder.IsVisible = false;
+                ZatPrigStack.IsVisible = true;
+                ZatPrigUnder.IsVisible = true;
+            };
+            ZatPrigLabel.GestureRecognizers.Add(ZatPrigGestureRecognizer);
+        }
+    }
+}
