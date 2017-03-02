16ef360a0b458fabe54658b135df73ca3b19dbbf
Vedran <vedran@samorazvoj.hr>
2017-03-02 14:40:49 +0100

    ..

--- PrigovorHR/PrigovorHR/Shared/Controllers/ExceptionController.cs
+++ PrigovorHR/PrigovorHR/Shared/Controllers/ExceptionController.cs
@@ -8,9 +8,15 @@ namespace PrigovorHR.Shared.Controllers
 {
     public class ExceptionController 
     {
-        public ExceptionController(Exception ex, string customermessage, string developermessage)
+        //Zadatak exception controlera je podignutu iznimku hendlati.
+        //Prvo prikazuje customermessage korisniku, developermessage sastavi za programere te iz ex-a izvuče podatke o grešci za developera
+        //Sve to pošalje bruni na API server a bruno za mene na moj email svako 12 sati šalje izvještaj o iznimkama.
+        //Uz to šaljem datum događaja, ekran na kojem se desio?? (kako??) , i informacije o deviceu i dostupnosti interneta.
+        public static void HandleException(Exception ex, string customermessage, string developermessage)
         {
-            Acr.UserDialogs.UserDialogs.Instance.Alert(ex.ToString());
+           var Base = ex.GetBaseException()?.ToString();
+           var Inner = ex.InnerException?.ToString(); 
+          //  Acr.UserDialogs.UserDialogs.Instance.Alert(ex.ToString());
         }
     }
 }
