3357903fe024efc7e4bbbd26c27060f07c723bf7
Saša Zrna <sazrna@vsite.hr>
2017-03-09 15:48:17 +0100

    1. preuređen CloseComplaintPage 2. dodan ListOfOtherStoresPage(treba ga povezat sa NavBarom) 3. dodan ListOfElementSectionView(odjeli) koji je zasad ugašen jer neznamo šta ćemo s njim 4. namješteni razni detalji po raznim straicama i vieowima kako sam nailazio na njih -----trebaš mi javiti kako da kod još više optimiziram da ti je lakše s njim manipulirati, sigurno ima još detalja koje ćes uočit pa mi ih javi što prije da mogu reagirati na vrijeme----

---
+++ PrigovorHR/PrigovorHR/Shared/Views/ListOfElementSectionsView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace PrigovorHR.Shared.Views
+{
+    public partial class ListOfElementSectionsView : ContentView
+    {
+        public ListOfElementSectionsView()
+        {
+            InitializeComponent();
+           
+        }
+    }
+}
