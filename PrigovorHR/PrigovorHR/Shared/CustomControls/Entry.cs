f768c46945ddf34730932cd5767856ff52b17e4d
Saša Zrna <sazrna@vsite.hr>
2017-04-04 10:39:51 +0200

    ..

--- PrigovorHR/PrigovorHR/Shared/CustomControls/Entry.cs
+++ PrigovorHR/PrigovorHR/Shared/CustomControls/Entry.cs
@@ -19,8 +19,43 @@ namespace PrigovorHR.Shared.CustomControls
             lblLabel.TextColor = Color.FromHex("#ff7e65");
             lblLabel.FadeTo(0, 0);
             lblLabel.TranslateTo(0, 40, 0);
-            Focused += Entry_Focused;
+            //Focused += Entry_Focused;
+            TextChanged += Entry_TextChanged;
             Unfocused += Entry_Unfocused;
+        }
+
+        bool LabelVisible = false;
+        private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!IsFocused) return;
+
+            if (Text.Length > 0 & !LabelVisible)
+            {
+                LabelVisible = true;
+                var StackChildren = ((StackLayout)Parent).Children;
+
+                if (EntryPositionInStack == 0)
+                {
+                    for (int i = 0; i < StackChildren.Count; i++)
+                        if (StackChildren[i] == this)
+                        {
+                            EntryPositionInStack = i;
+                            break;
+                        }
+
+                    ((StackLayout)Parent).Children.Insert(EntryPositionInStack, lblLabel);
+                }
+
+                lblLabel.Text = Placeholder;
+                lblLabel.FadeTo(1, 200);
+                lblLabel.TranslateTo(0, 20, 200);
+            }
+            else if(Text.Length<1 )
+            {
+                LabelVisible = false;
+                lblLabel.FadeTo(0, 200);
+                lblLabel.TranslateTo(0, 40, 200);
+            }
         }
 
         private  void Entry_Unfocused(object sender, FocusEventArgs e)
